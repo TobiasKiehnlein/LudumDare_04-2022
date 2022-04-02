807fe86f9bd301e6fbb7edc46de95df1f44a2a55
Tobias Kiehnlein <tobias@kiehnlein.com>
2022-04-02 15:59:59 +0200

    Create Main Menu

---
+++ LudumDare-04-2022/Assets/Scripts/Utils/SceneUtils.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utils
+{
+    public class SceneUtils : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator crossFade;
+
+        public void LoadNextScene()
+        {
+            var scene = SceneManager.GetActiveScene();
+            var nextLevelBuildIndex = (scene.buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            LoadScene(nextLevelBuildIndex);
+        }
+
+        private async void LoadScene(int sceneIndex)
+        {
+            crossFade.SetTrigger("Start");
+            await Task.Delay(1000);
+            SceneManager.LoadScene(sceneIndex);
+            crossFade.SetTrigger("Stop");
+        }
+
+        public void LoadMenuScene()
+        {
+            LoadScene(0);
+        }
+    
+    
+    }
+}
