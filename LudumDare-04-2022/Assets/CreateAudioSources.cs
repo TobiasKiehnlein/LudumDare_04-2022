e8fb2577a935fb5e6b4c2800ce3b5047d68baac1
Tobias Kiehnlein <tobias@kiehnlein.com>
2022-04-03 16:02:11 +0200

    Audio Handling

---
+++ LudumDare-04-2022/Assets/CreateAudioSources.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[Serializable]
+public struct AudioEntry
+{
+    public AudioClip AudioClip;
+    public AudioMixerGroup AudioMixerGroup;
+}
+
+public class CreateAudioSources : MonoBehaviour
+{
+    [SerializeField] private List<AudioEntry> audioEntries;
+
+    private void Awake()
+    {
+        foreach (var audioEntry in audioEntries)
+        {
+            var audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = audioEntry.AudioClip;
+            audioSource.loop = true;
+            audioSource.outputAudioMixerGroup = audioEntry.AudioMixerGroup;
+            audioSource.Play();
+        }
+    }
+}
